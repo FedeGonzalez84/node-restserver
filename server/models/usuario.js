eff5ba87f5f1e1240e0d87539fe2ff4d8dcb1d9b
Fede <fedegonzalez_84@hotmail.com>
2018-04-30 19:08:58 -0300

    RestServer - usuario

---
+++ server/models/usuario.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let Schema = mongoose.Schema;
+
+let rolesValidos = {
+    values: ['USER_ROLE', 'ADMIN_ROLE'],
+    message: '{VALUE} no es un rol válido'
+};
+
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email: {
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'El password es obligatorio']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+//Para mandarle un mensaje mas User friendly cuando el campo debe ser único
+usuarioSchema.plugin(uniqueValidator, { message: ' {PATH} debe ser único!' });
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
